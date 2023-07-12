eacacf26128fdaf6d32437c2db6e0ffcb7bf0c5b
Toh Hong Xiang <tohhongxiang@gmail.com>
2023-07-13 03:33:08 +0800

    Use plane instead of cube for water

--- Assets/Scripts/WaterGenerator.cs
+++ Assets/Scripts/WaterGenerator.cs
@@ -5,16 +5,21 @@ public class WaterGenerator : TerrainObjectGenerator
     public GameObject waterPrefab;
     [Range(0, 1)] public float waterHeight;
 
-    public override void SpawnObjects(Bounds waterBounds)
+    public override void SpawnObjects(Bounds bounds)
     {
         ClearObjects();
 
-        Vector3 chunkCenterWithoutVertical = new Vector3(waterBounds.center.x, 0, waterBounds.center.z);
+        Vector3 chunkCenterWithoutVertical = new Vector3(bounds.center.x, 0, bounds.center.z);
         GameObject spawnedWater = Instantiate(waterPrefab, chunkCenterWithoutVertical, Quaternion.identity, gameObject.transform);
 
-        Vector3 spawnedWaterScale = waterBounds.size;
-        spawnedWaterScale.y *= waterHeight;
+        // a plane with the same scale as a cube, is 10 times as large
+        int planeSizeMultiplier = 10;
+        Vector3 spawnedWaterScale = bounds.size / planeSizeMultiplier;
+        spawnedWaterScale.y = 1;
         spawnedWater.transform.localScale = spawnedWaterScale;
+
+        float spawnedWaterHeight = Mathf.Lerp(bounds.min.y, bounds.max.y, waterHeight);
+        spawnedWater.transform.position = new Vector3(spawnedWater.transform.position.x, spawnedWaterHeight, spawnedWater.transform.position.z);
     }
 
     public override void ClearObjects()
